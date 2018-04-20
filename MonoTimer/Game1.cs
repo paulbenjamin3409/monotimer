f8d2a48c5f0fe8382d2010483602f8810219ff6b
Paul Johnson Admin <pbjadmin@reliamax.com>
2018-04-20 15:55:03 -0500

    Add project files.

---
+++ MonoTimer/Game1.cs
@@ -0,0 +1,124 @@
+﻿#region Using Statements
+using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+#endregion
+
+namespace MonoTimer
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Vector2 position = new Vector2(50, 400);
+        SpriteFont font;
+        string time;
+        TimeSpan timer, eTime, cTime;
+        
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            graphics.IsFullScreen = true;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            font = Content.Load<SpriteFont>("BaseFont");
+
+            //TODO: use this.Content to load your game content here 
+        }
+
+        int counter = 1;
+
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            spriteBatch.Dispose();
+
+        }
+
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // For Mobile devices, this logic will close the Game when the Back button is pressed
+            // Exit() is obsolete on iOS
+#if !__IOS__ && !__TVOS__
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+                Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
+                Exit();
+            }
+#endif
+
+            timer += gameTime.ElapsedGameTime;
+            
+            eTime = gameTime.TotalGameTime;
+            
+            if (eTime.Minutes < 10)                
+                time = "Time - 0" + (int)(eTime.Minutes);
+            
+            else
+                time = "Time - " + (int)(eTime.Minutes);
+            
+            if (eTime.Seconds < 10)                
+                time += ":0" + (int)(eTime.Seconds);
+            
+            else
+                time += ":" + (int)(eTime.Seconds);
+
+
+            // TODO: Add your update logic here			
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            spriteBatch.DrawString(font, timer.ToString(), position, Color.Red);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
